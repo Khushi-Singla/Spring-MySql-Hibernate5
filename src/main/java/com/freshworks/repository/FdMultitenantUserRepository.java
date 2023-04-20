9040eb7fb602146751f0617b5a82a819e07df65c
Khushi Singla <khushi.singla@freshworks.com>
2023-04-20 11:51:55 +0530

    Added Classes

---
+++ src/main/java/com/freshworks/repository/FdMultitenantUserRepository.java
@@ -0,0 +1,24 @@
+package com.freshworks.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.freshworks.entities.FdMultitenantUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FdMultitenantUserRepository
+        extends JpaRepository<FdMultitenantUser, Long> {
+//    @Cacheable(value = RedisCacheNames.USER_BY_ACCOUNT_AUTH, unless = "#result == null")
+    Optional<FdMultitenantUser> findFirstByAccountIdAndAuthenticationToken(
+            Long accountId, String authenticationToken);
+
+    Optional<FdMultitenantUser> findByIdAndAccountId(Long id, Long accountId);
+
+//    @Cacheable(value = RedisCacheNames.USERS_BY_ACCOUNT, unless = "#result == null")
+    @Query("select u from FdMultitenantUser u inner join u.userProfile up where up.isForgotten = false " +
+        "and u.accountId = up.accountId and u.accountId=:accountId")
+    List<FdMultitenantUser> findAllActiveUsers(Long accountId);
+}
