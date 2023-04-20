db4f891f3b8fe8b54fbc81180561e293ab800589
Khushi Singla <khushi.singla@freshworks.com>
2023-04-20 11:11:25 +0530

    Classes Added

---
+++ src/main/java/com/freshworks/entities/FdMultitenantUser.java
@@ -0,0 +1,66 @@
+package com.freshworks.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "fd_multitenant_users")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class FdMultitenantUser
+        implements Serializable {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "account_id")
+    private Long accountId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "password_digest")
+    private String passwordDigest;
+    @Column(name = "authentication_token")
+    private String authenticationToken;
+    @Column(name = "confirmation_token")
+    private String confirmationToken;
+    @Column(name = "confirmed")
+    private Boolean confirmed;
+    @Column(name = "account_admin")
+    private Boolean accountAdmin;
+    @Column(name = "twitter_id")
+    private String twitterId;
+    @Column(name = "facebook_id")
+    private String facebookId;
+    @Column(name = "freshid_mapping_status")
+    private Integer freshidMappingStatus;
+    @Column(name = "inviter_id")
+    private Long inviterId;
+    @CreatedDate
+    @Column(name = "created_at")
+    private Instant createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @Fetch(FetchMode.JOIN)
+    private UserProfile userProfile;
+}
---
+++ src/main/java/com/freshworks/entities/UserProfile.java
@@ -0,0 +1,80 @@
+package com.freshworks.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfile
+        implements Serializable {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "account_id")
+    private Long accountId;
+    @Column(name = "job_title")
+    private String jobTitle;
+    @Column(name = "work_number")
+    private String workNumber;
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+    @Column(name = "language")
+    private String language;
+    @Column(name = "time_zone")
+    private String timeZone;
+    @Column(name = "last_login_at")
+    private Instant lastLoginAt;
+    @Column(name = "is_active")
+    private Boolean isActive;
+    @Column(name = "access_scope")
+    private Integer accessScope;
+    @Column(name = "logout_at")
+    private Instant logoutAt;
+    @Column(name = "deal_pipeline_id")
+    private Long dealPipelineId;
+    @Column(name = "configs")
+    private String configs;
+    @Column(name = "report_access")
+    private Boolean reportAccess;
+    @Column(name = "mobile_logout_at")
+    private Instant mobileLogoutAt;
+    @Column(name = "freshchat_restore_id")
+    private String freshchatRestoreId;
+    @Column(name = "forget_entity_access")
+    private Boolean forgetEntityAccess;
+    @Column(name = "is_forgotten")
+    private Boolean isForgotten;
+    @Column(name = "report_export")
+    private Boolean reportExport;
+    @CreatedDate
+    @Column(name = "created_at")
+    private Instant createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @OneToOne
+    @MapsId("id")
+    @JsonBackReference
+    @JoinColumn(name="id", referencedColumnName = "id")
+    private FdMultitenantUser user;
+}
